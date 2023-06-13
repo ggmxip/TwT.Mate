e58e8a1e9534218f800ee4aec07722705de3d6d3
ggmxip <92012598+ggmxip@users.noreply.github.com>
2023-06-14 03:58:50 +0530

    idk man 1 hr into this for a not soo good cursor/blinker effect

--- login-twitter.js
+++ login-twitter.js
@@ -33,3 +33,59 @@ $(document).ready(function() {
     //     })    
     //   });
     // })
+
+
+    // js for the type writer :)
+// JavaScript code
+// const text = "Welcome to TwT.Mate";
+// let index = 0;
+
+// function typeWriter() {
+//   if (index < text.length) {
+//     document.getElementById("typewriter").innerHTML += text.charAt(index);
+//     index++;
+//     setTimeout(typeWriter, 100);
+//   } else {
+//     setTimeout(eraseText, 3000);
+//   }
+// }
+
+// function eraseText() {
+//   if (index >= 0) {
+//     document.getElementById("typewriter").innerHTML = "";
+//     index = 0;
+//     setTimeout(typeWriter, 100);
+//   }
+// }
+
+// typeWriter();
+
+const text = "Welcome to TwT.Mate";
+let index = 0;
+let eraseMode = false;
+
+function typeWriter() {
+  const typewriterElement = document.getElementById("typewriter");
+  
+  if (index < text.length && !eraseMode) {
+    typewriterElement.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, 100);
+  } else if (index >= 0 && eraseMode) {
+    typewriterElement.innerHTML = text.substring(0, index);
+    index--;
+    setTimeout(typeWriter, 50);
+  } else if (index >= text.length && !eraseMode) {
+    eraseMode = true;
+    setTimeout(typeWriter, 2000);
+  } else if (index < 0 && eraseMode) {
+    eraseMode = false;
+    index = 0;
+    setTimeout(typeWriter, 100);
+  }
+}
+
+typeWriter();
+
+
+
